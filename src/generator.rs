70e8e7cb32d7a5724d6c7b1549cbcd5fcd59c2a8
Mohammad Danesh <mohammad@danesh.dev>
2021-06-04 01:30:30 +0430

    Merge generate_large and generate_medium into generate_icon function

--- src/generator.rs
+++ src/generator.rs
@@ -2,121 +2,126 @@ use crate::pixels::{LARGE_PIXELS, MEDIUM_PIXELS};
 use image::{ImageBuffer, ImageError, Rgba, RgbaImage};
 use std::{convert::TryInto, env, fs};
 
-fn separate_digits(num: usize) -> Vec<usize> {
-    fn separate(num: usize, digits: &mut Vec<usize>) {
-        if num >= 10 {
-            separate(num / 10, digits);
-        }
-        digits.push(num % 10);
-    }
-
-    let mut digits = Vec::new();
-    separate(num, &mut digits);
-    digits
+pub struct Icon {
+    digits: Vec<usize>,
+    shift: usize,
+    vertical_margin: usize,
+    horizontal_margin: usize,
+    thin_width: usize,
+    normal_width: usize,
+    pixel: Rgba<u8>,
+    name: String,
 }
 
-fn count_one(digits: &Vec<usize>) -> usize {
-    let mut one_count = 0;
-    for digit in digits {
-        if *digit == 1 {
-            one_count += 1;
-        }
-    }
+impl Icon {
+    pub fn generate() {
+        let pixel = Rgba::from([255; 4]);
 
-    one_count
-}
+        for i in 0..10000 {
+            let digits = Self::separate_digits(i);
+            let name = i.to_string() + ".ico";
 
-fn generate_large(digits: Vec<usize>, img: &mut RgbaImage, pixel: Rgba<u8>) {
-    let one_count = count_one(&digits);
-    let vert_margin = 8;
-    let mut horiz_margin = 4 + (2 * one_count);
-    let mut digit_width;
-
-    for digit in digits.iter() {
-        for y in 0..8 {
-            for x in 0..5 {
-                if LARGE_PIXELS[*digit][y] & 1 << x != 0 {
-                    let x_pos: u32 = ((4 - x) * 2 + horiz_margin).try_into().unwrap();
-                    let y_pos: u32 = (y * 2 + vert_margin).try_into().unwrap();
-
-                    img.put_pixel(x_pos, y_pos, pixel);
-                    img.put_pixel(x_pos, y_pos + 1, pixel);
-                    img.put_pixel(x_pos + 1, y_pos, pixel);
-                    img.put_pixel(x_pos + 1, y_pos + 1, pixel);
+            match digits.len() {
+                1 => continue,
+                2 => Self {
+                    digits,
+                    shift: 5,
+                    vertical_margin: 8,
+                    horizontal_margin: 4,
+                    thin_width: 6,
+                    normal_width: 10,
+                    pixel,
+                    name,
                 }
+                .generate_icon(LARGE_PIXELS),
+
+                3 => Self {
+                    digits,
+                    shift: 4,
+                    vertical_margin: 10,
+                    horizontal_margin: 2,
+                    thin_width: 4,
+                    normal_width: 8,
+                    pixel,
+                    name,
+                }
+                .generate_icon(MEDIUM_PIXELS),
+
+                4 => continue,
+                _ => (),
+            }
+        }
+    }
+
+    fn separate_digits(num: usize) -> Vec<usize> {
+        fn separate(num: usize, digits: &mut Vec<usize>) {
+            if num >= 10 {
+                separate(num / 10, digits);
+            }
+            digits.push(num % 10);
+        }
+
+        let mut digits = Vec::new();
+        separate(num, &mut digits);
+        digits
+    }
+
+    fn generate_icon<const N: usize>(&mut self, font: [[u8; N]; 10]) {
+        let mut ico = ImageBuffer::new(32, 32);
+        let mut margin = self.horizontal_margin + (2 * self.count_one());
+
+        for &digit in self.digits.iter() {
+            for y in 0..font[digit].len() {
+                for x in (0..self.shift).rev() {
+                    if font[digit][y] & 1 << x != 0 {
+                        let x_pos: u32 = ((self.shift - x - 1) * 2 + margin)
+                            .try_into()
+                            .expect("X pos type conversion failed");
+                        let y_pos: u32 = (y * 2 + self.vertical_margin)
+                            .try_into()
+                            .expect("Y pos type conversion failed");
+
+                        ico.put_pixel(x_pos, y_pos, self.pixel);
+                        ico.put_pixel(x_pos + 1, y_pos, self.pixel);
+                        ico.put_pixel(x_pos, y_pos + 1, self.pixel);
+                        ico.put_pixel(x_pos + 1, y_pos + 1, self.pixel);
+                    }
+                }
+            }
+
+            if digit == 1 {
+                margin += self.thin_width + self.horizontal_margin;
+            } else {
+                margin += self.normal_width + self.horizontal_margin;
             }
         }
 
-        if *digit == 1 {
-            digit_width = 6;
-        } else {
-            digit_width = 10;
-        }
-        horiz_margin += digit_width + 4;
+        self.save_icon(&ico);
     }
-}
 
-fn generate_medium(digits: Vec<usize>, img: &mut RgbaImage, pixel: Rgba<u8>) {
-    let one_count = count_one(&digits);
-    let vert_margin = 10;
-    let mut horiz_margin = 2 + (2 * one_count);
-    let mut digit_width;
-
-    for digit in digits.iter() {
-        for y in 0..6 {
-            for x in (0..4).rev() {
-                if MEDIUM_PIXELS[*digit][y] & 1 << x != 0 {
-                    let x_pos: u32 = ((3 - x) * 2 + horiz_margin).try_into().unwrap();
-                    let y_pos: u32 = (y * 2 + vert_margin).try_into().unwrap();
-
-                    img.put_pixel(x_pos, y_pos, pixel);
-                    img.put_pixel(x_pos, y_pos + 1, pixel);
-                    img.put_pixel(x_pos + 1, y_pos, pixel);
-                    img.put_pixel(x_pos + 1, y_pos + 1, pixel);
-                }
+    fn count_one(&self) -> usize {
+        let mut one_count = 0;
+        for &digit in &self.digits {
+            if digit == 1 {
+                one_count += 1;
             }
         }
 
-        if *digit == 1 {
-            digit_width = 4;
-        } else {
-            digit_width = 8;
-        }
-        horiz_margin += digit_width + 2;
+        one_count
     }
-}
 
-fn save_icon(file_name: &String, img: &RgbaImage) {
-    let mut temp_dir = env::temp_dir();
-    temp_dir.push("Pingopher");
-    temp_dir.push(&file_name);
+    fn save_icon(&self, ico: &RgbaImage) {
+        let mut temp_dir = env::temp_dir();
+        temp_dir.push("Pingopher");
+        temp_dir.push(&self.name);
 
-    img.save(&temp_dir).unwrap_or_else(|err| match err {
-        ImageError::IoError(_) => {
-            temp_dir.pop();
-            fs::create_dir_all(&temp_dir).unwrap();
-            save_icon(&file_name, &img);
-        }
-        _ => panic!("Saving failed: {}", err), // TODO: Return error to the user
-    });
-}
-
-pub fn generate_icons() {
-    let pixel = Rgba::from([255; 4]);
-
-    for i in 0..10000 {
-        let digits = separate_digits(i);
-        let file_name = i.to_string() + ".ico";
-        let mut img = ImageBuffer::new(32, 32);
-
-        match digits.len() {
-            1 => continue,
-            2 => generate_large(digits, &mut img, pixel),
-            3 => generate_medium(digits, &mut img, pixel),
-            4 => continue,
-            _ => println!("Not Implemented"),
-        }
-
-        save_icon(&file_name, &img);
+        ico.save(&temp_dir).unwrap_or_else(|err| match err {
+            ImageError::IoError(_) => {
+                temp_dir.pop();
+                fs::create_dir_all(&temp_dir).unwrap();
+                self.save_icon(&ico);
+            }
+            _ => panic!("Saving failed: {}", err), // TODO: Return error to the user
+        });
     }
 }
