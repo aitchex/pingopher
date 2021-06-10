7628c60df355b986efa9cbff71e06939c5363ae0
Mohammad Danesh <mohammad@danesh.dev>
2021-06-10 20:57:47 +0430

    Hide ping console

--- src/ping.rs
+++ src/ping.rs
@@ -1,8 +1,11 @@
-use std::process::Command;
+use std::{os::windows::process::CommandExt, process::Command};
+
+const CREATE_NO_WINDOW: u32 = 0x08000000;
 
 pub fn get_rtt(ip: &String) -> String {
     let output = Command::new("cmd")
         .args(&["/C", "ping -n 1", ip])
+        .creation_flags(CREATE_NO_WINDOW)
         .output()
         .expect("Failed to execute process");
     let output = String::from_utf8(output.stdout).unwrap();
